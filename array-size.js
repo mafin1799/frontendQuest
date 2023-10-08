4ed49a112f4cb5a03edbe6a7b01e9a1f383132da
maffin1799 <nik.chujko@mail.ru>
2023-10-08 19:03:03 +0300

    array.size without length

--- array-size.js
+++ array-size.js
@@ -1,6 +1,16 @@
+const MAX_INT32 = 4294967295;
+
 Object.defineProperty(Array.prototype, 'size', {
     get: function () {
-        return this.length;
+        let size = 0;
+
+        for (let i in this) {
+            if (this.hasOwnProperty(i)) {
+                size++;
+            }
+        }
+
+        return size;
     },
     set: function (size) {
         if (!(typeof size === 'number' | size instanceof Number)) {
@@ -9,12 +19,21 @@ Object.defineProperty(Array.prototype, 'size', {
         if (size < 0) {
             throw new RangeError('Недопустимый размер массива');
         }
-        this.length = size;
+        if (size > MAX_INT32) {
+            throw new RangeError('Превышен максимальный размер массива');
+        }
+        if (size < this.size) {
+            this.splice(size);
+        } else if (size > this.size) {
+            for (let i = this.size; i < size; i++) {
+                this[i] = null;
+            }
+        }
     },
 });
 
-console.log([0,1].size);
+console.log([0, 1].size);
 
 let array = ['0', '2', '5'];
-array.size = 0;
+array.size = 9999999999999;
 console.log(array);
